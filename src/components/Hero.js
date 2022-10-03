c7a58ed53b8ca68bb54353a428f3af627b215d31
b0p1 <sofiarojo09@gmail.com>
2022-10-02 21:07:05 -0400

    add hero image and styling for all breakpoints

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,10 +1,10 @@
+import '../styles/hero.scss'
 
-
-export default function Hero() {
+export default function Hero({selectedVideo}) {
+  console.log(selectedVideo);
   return (
     <section className="hero">
-      <video className="hero__video">
-        <source />
+      <video className="hero__video" controls poster={selectedVideo.image}> 
       </video>
     </section>
   );

90638d1bc3907a0953fbb7e61a841a0520401ab4
b0p1 <sofiarojo09@gmail.com>
2022-10-09 15:35:21 -0400

    fix some styles in video desc and bar desktop

--- src/components/main/VideoSection/VideoDetails.js
+++ src/components/main/VideoSection/VideoDetails.js
@@ -4,20 +4,21 @@ import ViewIcon from "../../../assets/icons/views.svg";
 import LikesIcon from "../../../assets/icons/likes.svg";
 
 function VideoDetails({ selectedVideo }) {
+
   return (
     <div className="video-section__details">
-      <h1>{selectedVideo.title}</h1>
+      <h1 >{selectedVideo.title}</h1>
       <div className="video-section__data">
         <div className="video-section__top-row">
           <h2 className="video-section__channel">By {selectedVideo.channel}</h2>
-          <img className="video-section__views-icon" src={ViewIcon} />
+          <img className="video-section__views-icon" src={ViewIcon}/>
           <h2 className="video-section__views">{selectedVideo.views}</h2>
         </div>
         <div className="video-section__bottom-row">
           <h2 className="video-section__date">
             {formatDate(new Date(selectedVideo.timestamp))}
           </h2>
-          <img className="video-section__likes-icon" src={LikesIcon} />
+          <img className="video-section__likes-icon" src={LikesIcon}/>
           <h2 className="video-section__likes">{selectedVideo.likes}</h2>
         </div>
       </div>

11860d69373149acba4e1b33db9089a55a5f5c5e
b0p1 <sofiarojo09@gmail.com>
2022-10-11 17:04:59 -0400

    begin to refactor files

--- src/pages/VideoPage.js
+++ src/pages/VideoPage.js
@@ -2,7 +2,7 @@ import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 import axios from "axios";
 import Header from "../components/Header";
-import Main from "../components/main/Main/Main";
+import Main from "../components/MainSection/Main/Main";
 import Hero from "../components/Hero";
 
 function VideoPage() {
